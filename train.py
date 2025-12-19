7d2c4d3dc74f60b42a5b79378155c3d6a7a486c0
Abdallah <abdallah.ashraf854@gmail.com>
2025-12-19 23:43:42 +0200

    model training script

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, QuantileTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, QuantileTransformer, PolynomialFeatures
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import cross_val_score, KFold
 from helper import load_data, model_evaluation
@@ -27,7 +27,8 @@ def train_ridge_model():
                             'hour','store_and_fwd_flag']
     continuous_features = ['pickup_longitude', 'pickup_latitude', 'dropoff_longitude',
                                    'dropoff_latitude', 'haversine_distance','direction_deg']
-    other_features = ['winter', 'spring', 'summer', 'autumn']
+    other_features = ['winter', 'spring', 'summer', 'autumn', 'rush_hour', 'rush_day', '(1st/8)',
+                      '(2nd/8)', '(3rd/8)','(4th/8)', '(5th/8)','(6th/8)', '(7th/8)', '(8th/8)']
 
     train_features = features_to_encode + continuous_features + other_features
 
@@ -36,7 +37,7 @@ def train_ridge_model():
         ('encoder', OneHotEncoder(handle_unknown='ignore'), features_to_encode),
         ('quantile_trans', QuantileTransformer(output_distribution='normal'), continuous_features),
         ('scaler', StandardScaler(), continuous_features)
-    ], remainder='drop')
+    ], remainder='passthrough')
 
     # pipeline of training (transformations, model algorithm)
     pipeline = Pipeline(steps=[("col_trans", col_trans), ("model", Ridge(alpha=1, random_state=RANDOM_STATE))])
@@ -46,8 +47,8 @@ def train_ridge_model():
     rmse_train, score_train = model_evaluation(model, train_df[train_features], train_df[TARGET_FEATURE])
     rmse_val, score_val = model_evaluation(model, val_df[train_features], val_df[TARGET_FEATURE])
 
-    print(f'RMSE (train): {rmse_train:.2f}   -   R2 Score (train): {score_train:.2f}')
-    print(f'RMSE (val): {rmse_val:.2f}    -   R2 Score (val): {score_val:.2f}')
+    print(f'RMSE(train): {rmse_train:.2f}          -   RMSE(val): {rmse_val:.2f}')
+    print(f'R2 Score(train): {score_train:.2f}     -   R2 Score(val): {score_val:.2f}')
 
 
     # Cross Validation API shows scores & variance of different models
@@ -61,5 +62,5 @@ def train_ridge_model():
 
 if __name__ == '__main__':
     # RMSE(train)=0.48,         RMSE(val)=0.48
-    # r2_score(train)=0.64      r2_score(val)=0.64
+    # r2_score(train)=0.66      r2_score(val)=0.65
     train_ridge_model()
