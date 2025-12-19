098e3a3ecedf37c72f862e0aa185173600327071
Abdallah <abdallah.ashraf854@gmail.com>
2025-12-19 02:17:04 +0200

    helper fuctions script

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+
 def load_data(train_file_path:str='split/train.csv', val_file_path:str='split/val.csv'):
     '''
     :param train_file_path:
@@ -11,34 +12,16 @@ def load_data(train_file_path:str='split/train.csv', val_file_path:str='split/va
 
     return train_df, val_df
 
-def preprocess_data(data, preprocessing_option=1):
-    '''
-    preprocessing on data according to option:
-    1 for MinMaxScaler()
-    2 for standardScaler()
-    None for other options
-    '''
-    from sklearn.preprocessing import MinMaxScaler, StandardScaler
-
-    processor = None
-    if preprocessing_option == 1:
-        processor = MinMaxScaler()
-        return processor, processor.fit_transform(data)
-    elif preprocessing_option == 2:
-        processor = StandardScaler()
-        return processor, processor.fit_transform(data)
-
-    return processor, data
-
 
 def model_evaluation(model, x, t):
     '''model evaluation using rmse & r2 score'''
+    import numpy as np
     from sklearn.metrics import r2_score, mean_squared_error
 
     preds = model.predict(x)
-    rmse = mean_squared_error(t, preds, squared=False)
+    mse = mean_squared_error(t, preds)
     score = r2_score(t, preds)
 
-    return rmse, score
+    return np.sqrt(mse), score
 
 
