7cfb5c54e6e1f4a2e4cca3273679412a6fd4761f
Abdallah <abdallah.ashraf854@gmail.com>
2026-01-27 15:40:35 +0200

    helper functions script

--- helper.py
+++ helper.py
@@ -1,16 +1,14 @@
 
-def load_data(train_file_path:str='split/train.csv', val_file_path:str='split/val.csv'):
+def load_data(file_path):
     '''
-    :param train_file_path:
-    :param val_file_path:
-    :return: train & val data
+    :param file_path:
+    :return: loaded data
     '''
     import pandas as pd
 
-    train_df = pd.read_csv(train_file_path)
-    val_df = pd.read_csv(val_file_path)
+    loaded_data = pd.read_csv(file_path)
 
-    return train_df, val_df
+    return loaded_data
 
 
 def model_evaluation(model, x, t):
